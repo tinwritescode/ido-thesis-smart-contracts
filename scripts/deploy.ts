e4704e51bcf2843ddec9384f46fd1332ab8873bd
Tin Nguyen <hopthucuatin@gmail.com>
2023-03-23 20:51:50 +0700

    feat: save more to env

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -32,6 +32,9 @@ async function main() {
     NEXT_PUBLIC_STAKING_CONTRACT_ADDRESS: stakingContract.address,
     NEXT_PUBLIC_STAKING_TOKEN_ADDRESS: stakingTokenAddress,
     NEXT_PUBLIC_REWARD_TOKEN_ADDRESS: rewardTokenAddress,
+    NEXT_PUBLIC_USDC_TOKEN_ADDRESS: usdcToken.address,
+    NEXT_PUBLIC_IDO_CONTRACT_ADDRESS: idoContract.address,
+    NEXT_PUBLIC_IDO_TOKEN_ADDRESS: idoToken.address,
   });
 }
 
